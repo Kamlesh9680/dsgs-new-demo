6bc7d5d536e383884a4fdc19a47aeb94352392d0
Kamlesh Soni <141931074+Kamlesh9680@users.noreply.github.com>
2025-02-24 09:36:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'layout');
 app.use(express.static('public'));
-
+app.set("views", path.join(__dirname, "views"));
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home - DSGS' });
 });
